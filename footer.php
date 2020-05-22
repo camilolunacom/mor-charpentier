7883b6fa9daa4ca6e7556c8293837f5f9c34ead4
Camilo Luna <camilo@alunizar.co>
2020-05-22 09:15:07 -0500

    Add footer

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
+  </div> <!-- #content -->
 
-  <?php wp_footer(); ?>
+  <footer class="site-footer" role="contentinfo">
+
+    <p>Current exhibition</p>
+    <p>Contact email</p>
+    <p>Social media links</p>
+
+  </footer>
+
+</div> <!-- #page -->
+
+<?php wp_footer(); ?>
 
 </body>
 </html>
