bb9d4ba2fa8558e809f2e3f07bb8a80f3654cf66
Camilo Luna <camilo@alunizar.co>
2020-05-22 09:15:41 -0500

    Update header

--- header.php
+++ header.php
@@ -15,11 +15,11 @@
 
   <header class="main-header" role="banner">
 
-  <div class="main-header__logo">
-    <a href="<?php echo esc_rul( home_url( '/') ); ?>" role="home"></a>
-  </div>
+    <div class="main-header__logo">
+      <a href="<?php echo esc_url( home_url( '/') ); ?>" role="home">Logo</a>
+    </div>
 
-    <nav class="main-header__nav" role="navigation">
+    <nav class="main-header__nav" role="navigation" aria-label="Main">
       <?php
         $args = [
           'theme_location'  => 'primary',
@@ -30,4 +30,16 @@
       ?>
     </nav>
 
+    <div class=main-header__extra">
+      <nav class="main-header__lang-switcher" role="navigation" aria-label="Language switcher">
+        <?php do_action('wpml_add_language_selector'); ?>
+      </nav>
+
+      <nav class="main-header__dev-team" role="navigation" aria-label="Development team external links">
+        <a href="http://elmonocromo.com/" class="main-header__dev-link" target="_blank"><span>El Monocromo</span></a> + <a href="http://8manos.com/" class="main-header__dev-link" target="_blank"><span>8manos</span></a> + <a href="https://www.alunizar.co/" class="main-header__dev-link" target="_blank"><span>Alunizar</span></a>
+      </nav>
+    </div>
+
   </header>
+
+  <div id="content">
