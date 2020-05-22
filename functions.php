c4616a50947d9e7da05757212e5c15399431f43f
Camilo Luna <camilo@alunizar.co>
2020-05-22 09:12:00 -0500

    Register sidebars

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php
 
 // Setup Theme
-
-if ( ! function_exists( 'alzr_theme_setup' ) ) :
+if ( ! function_exists( 'alzr_theme_setup' ) ) {
   /**
    * Sets up theme defaults and registers support for various WordPress features.
    */
@@ -17,7 +16,7 @@ if ( ! function_exists( 'alzr_theme_setup' ) ) :
     /**
      * Add title tag to the head of pages
      */
-    add_theme_support( 'title_tag' );
+    add_theme_support( 'title-tag' );
 
     /**
      * Enable support for post thumbnails and featured images.
@@ -30,15 +29,38 @@ if ( ! function_exists( 'alzr_theme_setup' ) ) :
     add_theme_support( 'html5' );
   
     /**
-     * Add support for two custom navigation menus.
+     * Add support for custom navigation menus.
      */
     register_nav_menus( [
         'primary'   => esc_html__( 'Primary Menu', 'alzr' ),
     ] );
 
   }
-endif; // alzr_theme_setup
-add_action( 'after_setup_theme', 'alzr_theme_setup' );
+  add_action( 'after_setup_theme', 'alzr_theme_setup' );
+
+} // alzr_theme_setup
+
+// Setup widget areas.
+if ( ! function_exists( 'alzr_widgets_init' ) ) {
+
+  function alzr_widgets_init() {
+
+    $args = [
+      'name'          => esc_html__( 'News Sidebar', 'alzr' ),
+      'id'            => 'news-sidebar',
+      'description'   => esc_html__( 'Add widgets for news sidebar.', 'alzr'),
+      'before_widget' => '<section class="widget">',
+      'class'         => 'news-sidebar',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h2 class=news-sidebar__title">',
+      'after_title'   => '</h2>',
+    ];
+    register_sidebar( $args );
+
+  }
+  add_action( 'widgets_init', 'alzr_widgets_init' );
+
+} // alzr_widgets_init
 
 /**
  * Load JS and CSS
@@ -47,7 +69,5 @@ function alzr_enqueue_scripts() {
   wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css', [], time() );
 }
 add_action( 'wp_enqueue_scripts', 'alzr_enqueue_scripts');
-
-
 
 ?>
