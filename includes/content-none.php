5993c0a9f31bb7bcb14af6d7a20435b1a87bdbcc
Camilo Luna <camilo@alunizar.co>
2020-05-28 18:01:47 -0500

    Update content-none part

--- includes/content-none.php
+++ includes/content-none.php
@@ -8,7 +8,9 @@
 
   <div class="entry-content">
 
-    <p><?php esc_html_e( "We couldn't find any content.", 'alrz' ); ?></p>
+    <p><?php esc_html_e( "We couldn't find any content.", 'alzr' ); ?></p>
+
+    <p><a href="<?php echo esc_url( home_url( '/') ); ?>" role="home">Go to homepage</a></p>
 
   </div>
 
