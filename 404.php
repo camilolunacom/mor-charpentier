2b217dc1b45dd49347e26fcc63c0a129cf829631
Camilo Luna <camilo@alunizar.co>
2020-05-28 17:59:12 -0500

    Create 404 template

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main class="site-main" role="main">
+
+      <article <?php post_class(); ?>>
+
+        <div class="single__container">              
+
+          <header class="entry-header">
+
+            <h1><?php esc_html_e( 'Page not found', 'mc-2020'); ?></h1>
+
+          </header>
+
+          <div class="entry-content">
+
+            <p><?php esc_html_e( "We couldn't find the page you were looking for.", 'mc-2020'); ?></p>
+            <p><a href="<?php echo esc_url( home_url( '/') ); ?>" role="home"><?php esc_html_e( 'Go to homepage', 'mc-2020'); ?></a></p>
+            
+          </div>
+
+        </div>
+
+      </article>
+
+    </main>
+
+  </div> <!-- #primary -->
+
+  <h1>404.php</h1>
+
+<?php get_footer(); ?>
