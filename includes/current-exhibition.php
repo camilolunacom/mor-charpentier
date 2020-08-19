bcdcc1b46f2bd6e723a51cd40032f67c3268a5d7
Camilo Luna <camilo@alunizar.co>
2020-08-19 12:40:31 -0500

    Add footer included files

---
+++ includes/current-exhibition.php
@@ -0,0 +1,59 @@
+<?php
+
+  $today = date( 'Ymd' );
+
+  $last_exhibition = new WP_Query( array(
+    'post_type' => 'exhibition',
+    'posts_per_page' => 1,
+    'meta_query' => array(
+      array(
+        'key'     => 'end_date',
+        'compare' => '>=',
+        'value'   => $today
+      ),
+    ),
+  ) );
+
+?>
+
+<div class="footer-exhibition">
+
+<?php if ( $last_exhibition->have_posts() ) : ?>
+  <?php while ( $last_exhibition->have_posts() ) : $last_exhibition->the_post(); ?>
+
+    <h3 class="footer-exhibition__title"><?php _e( 'Current exhibition', 'mc2020' ); ?></h3>
+
+    <?php the_title('<h4 class="footer-exhibition__name">', '</h4>'); ?>
+
+    <?php $related_artists = get_field( 'relation_artists_exhibitions' ); ?>
+
+    <?php if ( $related_artists ) : ?>
+      <ul class="footer-exhibition__artists">
+        <?php foreach( $related_artists as $related_artist ): ?>
+          <li class="footer-exhibition__artist"><?php echo get_the_title( $related_artist->ID ); ?></li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+
+    <?php
+      $start_date_string = get_field( 'start_date' );
+      $end_date_string = get_field( 'end_date' );
+
+      $start_date_format = _x( 'F dS', 'Current exhibition start date format', 'mc2020' );
+      $end_date_format = _x( 'F dS, Y', 'Current exhibition end date format', 'mc2020' );
+
+      $start_date  = DateTime::createFromFormat('Ymd', $start_date_string);
+      $end_date  = DateTime::createFromFormat('Ymd', $end_date_string);
+    ?>
+
+    <p class="footer-exhibition__dates">
+      <?php echo $start_date->format( $start_date_format ); ?> – <?php echo $end_date->format( $end_date_format ); ?>
+    </p>
+
+  <?php endwhile; ?>
+  <?php wp_reset_postdata(); ?>
+<?php else : ?>
+  <h3 class="footer_exhibition__title"><?php _e( 'There are no current exhibitions', 'mc2020' ); ?></h3>
+<?php endif; ?>
+
+</div>
---
+++ includes/social-links.php
@@ -0,0 +1,21 @@
+<div class="sm-icons">
+
+  <a class="sm-icons__link" href="https://www.facebook.com/mor.charpentier" target="_blank">
+    <svg class="sm-icon__svg" viewbox="0 0 40 40">
+      <use xlink:href="#sm-facebook"></use>
+    </svg>
+  </a>
+
+  <a class="sm-icons__link" href="https://www.instagram.com/morcharpentier/" target="_blank">
+    <svg class="sm-icon__svg" viewbox="0 0 40 40">
+      <use xlink:href="#sm-instagram"></use>
+    </svg>
+  </a>
+
+  <a class="sm-icons__link" href="https://twitter.com/mor_charpentier" target="_blank">
+    <svg class="sm-icon__svg" viewbox="0 0 40 40">
+      <use xlink:href="#sm-twitter"></use>
+    </svg>
+  </a>
+
+</div>
