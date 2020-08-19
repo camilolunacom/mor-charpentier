fa654e08df0c96af02a8768a398d194d58a4412a
Camilo Luna <camilo@alunizar.co>
2020-08-19 12:36:06 -0500

    Add content to footer

--- footer.php
+++ footer.php
@@ -1,10 +1,33 @@
   </div> <!-- #content -->
 
-  <footer class="site-footer" role="contentinfo">
+  <footer class="footer" role="contentinfo">
 
-    <p>Current exhibition</p>
-    <p>Contact email</p>
-    <p>Social media links</p>
+    <?php
+
+    $part1 = '<a href="https://www.alunizar.co" target="_blank" rel="nofollow noopener">';
+    $part2 = '</a>';
+
+    ?>
+
+    <div class="footer__col footer__col--1">
+
+    <?php get_template_part( 'includes/current-exhibition' ); ?>
+
+    </div>
+
+    <div class="footer__col footer__col--2">
+      <p id="footer__info">
+        &copy; <?php echo date("Y"); ?> <a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>.<br>
+        <?php _e('All rights reserved.', 'alunizar-child'); ?><br>
+        <?php printf(__('By %1$sAlunizar%2$s.', 'alunizar-child'), $part1, $part2);?>
+      </p>
+    </div>
+
+    <div class="footer__col footer__col--3">
+
+      <?php get_template_part( 'includes/social-links' ); ?>
+
+    </div>
 
   </footer>
 
