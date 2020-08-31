9bb39239eac4c0b02821122fec264a1534e850fb
Camilo Luna <camilo@alunizar.co>
2020-08-31 17:43:53 -0500

    Update woocommerce options

--- functions.php
+++ functions.php
@@ -52,16 +52,15 @@ if ( ! function_exists( 'mc2020_theme_setup' ) ) {
 
     function mc2020_add_woocommerce_support() {
         add_theme_support( 'woocommerce', array(
-            'thumbnail_image_width' => 150,
-            'single_image_width'    => 300,
+            'thumbnail_image_width' => 708,
+            'single_image_width'    => 708,
     
             'product_grid'          => array(
-                'default_rows'    => 3,
-                'min_rows'        => 2,
-                'max_rows'        => 8,
-                'default_columns' => 4,
+                'default_rows'    => 1,
+                'min_rows'        => 1,
+                'default_columns' => 2,
                 'min_columns'     => 2,
-                'max_columns'     => 5,
+                'max_columns'     => 2,
             ),
         ) );
     }
