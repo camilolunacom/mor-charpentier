279d5bf9e994cb0688452674b6016b1f5b81b975
Camilo Luna <camilo@alunizar.co>
2020-08-31 16:11:29 -0500

    Replace Glide with Slick for sliders

--- front-page.php
+++ front-page.php
@@ -1,66 +1,62 @@
+<?php global $header_tone; ?>
+<?php $header_tone = get_field( 'header_tone' ); ?>
 <?php get_header(); ?>
+
+<?php
+$hero_height = get_field( 'hero_height' );
+$background_color = get_field( 'background_color' );
+$autoplay_speed = get_field( 'autoplay_speed' );
+$autoplay_speed_miliseconds = $autoplay_speed > 0 ? $autoplay_speed * 1000 : 0;
+?>
 
 <main <?php post_class( 'site-main' ); ?> role="main">
 
 	<!-- Hero slider -->
 	<section class="hero">
-		<div class="glide">
 
-			<div class="glide__track" data-glide-el="track">
+<div class="hero-slider<?php if ( $header_tone ) : ?> hero-slider--light<?php endif; ?>"<?php if( $autoplay_speed_miliseconds ) : ?> data-slick='{"autoplay": true, "autoplaySpeed": <?php echo $autoplay_speed_miliseconds; ?>}'" <?php endif; ?><?php if ( $background_color ) : ?>style="background-color: <?php echo $background_color ?>"<?php endif; ?>>
 
-				<ul class="glide__slides">
+			<?php if ( have_rows( 'slider' ) ) : while ( have_rows( 'slider' ) ) : the_row(); ?>
 
 				<?php
+				$background = get_sub_field( 'background' );
+				$title = get_sub_field( 'title' );
+				$text = get_sub_field( 'text' );
+				$button = get_sub_field( 'button' );
+				$button_text = get_sub_field( 'button_text' );
+				$button_url = get_sub_field( 'button_url' );
+				$overlay_color = get_sub_field( 'overlay_color' );
+				$overlay_opacity = get_sub_field( 'overlay_opacity' );
+				$overlay_opacity_decimal = $overlay_opacity > 0 ? $overlay_opacity / 100 : 0;
+				$text_color = get_sub_field( 'text_color' );
+				$background_size = get_sub_field( 'background_size' );
+				?>
 
-				if ( have_rows('slider') ) :
+<div class="hero-slider__slide<?php if ( $text_color ) :?> hero-slider__slide--light<?php endif; ?><?php if ( $background_size ) :?> hero-slider__slide--cover<?php endif; ?>""<?php if ( $background ) : ?> style="background-image: url(<?php echo $background; ?>);<?php endif; ?>">
+				<div class="hero-slider__overlay" style="<?php if ( $overlay_color ) : ?>background-color: <?php echo $overlay_color; ?>; <?php endif; ?><?php if ( $overlay_opacity ) : ?>opacity: <?php echo $overlay_opacity_decimal; ?>; <?php endif; ?>"></div>
 
-				while ( have_rows('slider') ) : the_row();
-					$background = get_sub_field( 'background' );
-					$title = get_sub_field( 'title' );
-					$text = get_sub_field( 'text' );
-					$button_text = get_sub_field( 'button_text' );
-					$button_url = get_sub_field( 'button_url' );
+					<div class="hero-slider__content"<?php if ( $hero_height ) : ?> style="min-height: calc(<?php echo $hero_height; ?>vh - 20px);<?php endif; ?>">
 
-					?>
+						<?php if ( $title ) : ?><h2 class="hero-slider__title"><?php echo $title; ?></h2><?php endif; ?>
 
-					<li class="glide__slide"<?php if ( $background ) : ?> style="background-image: url(<?php echo $background; ?>)"<?php endif; ?>>
-					<div class="glide__overlay"></div>
+						<?php if ( $text ) : ?><div class="hero-slider__text"><?php echo $text; ?></div><?php endif; ?>
 
-					<div class="glide__content">
-
-						<?php if ( $title ) : ?><h2 class="glide__title"><?php echo $title; ?></h2><?php endif; ?>
-
-						<?php if ( $text ) : ?><div class="glide__text"><?php echo $text; ?></div><?php endif; ?>
-
-						<?php if ( $button_text ) : ?><a class="glide__button btn btn--alt" href="<?php echo $button_url; ?>"><?php echo $button_text; ?></a><?php endif; ?>
+						<?php if ( $button ) : ?>
+							<a class="hero-slider__button btn" href="<?php echo $button_url; ?>"><?php echo $button_text; ?></a>
+						<?php endif; ?>
 
 					</div>
 
-					</li>
+				</div>
 
-				<?php endwhile; ?>
-				<?php else : ?>
+			<?php endwhile; else : ?>
 
-				<li class="glide__slide">
-					<h2 class="glide__title"><?php _e( 'mor charpentier', 'mc2020' ); ?></h2>
+				<li class="hero-slider__slide">
+					<h2 class="hero-slider__title"><?php _e( 'mor charpentier', 'mc2020' ); ?></h2>
 				</li>
 
-				<?php endif; ?>
+			<?php endif; ?>
 
-				</ul>
-			</div>
-
-			<div class="glide__arrows" data-glide-el="controls">
-				<button class="glide__arrow glide__arrow--left" data-glide-dir="<">
-				<svg class="slider-arrow" viewBox="0 0 200 330">
-					<use xlink:href="#arrow-left"></use>
-				</svg>
-				</button>
-				<button class="glide__arrow glide__arrow--right" data-glide-dir=">">
-				<svg class="slider-arrow" viewBox="0 0 200 330">
-					<use xlink:href="#arrow-right"></use>
-				</svg>
-				</button>
 			</div>
 
 		</div>
