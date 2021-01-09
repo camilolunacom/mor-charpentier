9204ae5f1398cff7b07bb5491c736b63b7772f6b
Camilo Luna <camilo@alunizar.co>
2021-01-09 11:29:26 -0500

    Update support for WooCommerce

--- functions.php
+++ functions.php
@@ -68,7 +68,8 @@ if ( ! function_exists( 'mc2020_theme_setup' ) ) {
                 'max_columns'     => 2,
             ),
         ) );
-        add_theme_support( 'wc-product-gallery-lightbox' );
+		add_theme_support( 'wc-product-gallery-zoom' );
+		add_theme_support( 'wc-product-gallery-slider' );
     }
     
     add_action( 'after_setup_theme', 'mc2020_add_woocommerce_support' );
